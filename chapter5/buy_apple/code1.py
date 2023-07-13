603084549731993892f8e84cddfd32482fa57591
Dongmin_Kim <dongmin0355@gmail.com>
2023-07-13 17:55:41 +0900

    chapter5 진행

---
+++ chapter5/buy_apple/code1.py
@@ -0,0 +1,32 @@
+# 곱셈 계층 구현
+class MulLayer:
+  def __init__(self):
+    self.x = None
+    self.y = None
+  
+  def forward(self, x, y):
+    self.x = x
+    self.y = y
+    out = x * y
+    
+    return out
+  
+  def backward(self, dout):
+    dx = dout * self.y # x와 y가 역전
+    dy = dout * self.x 
+    
+    return dx, dy
+  
+# 덧셈 계층 구현
+class AddLayer:
+  def __init__(self):
+    pass
+  
+  def forward(self, x, y):
+    out = x + y
+    return out
+  
+  def backward(self, dout):
+    dx = dout * 1
+    dy = dout * 1
+    return dx, dy
