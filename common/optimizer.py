2f7a798cf16356b17c2720d2665f6ac0fb802ea7
Dongmin_Kim <dongmin0355@gmail.com>
2023-07-18 16:49:18 +0900

    chapter6 학습 관련 기술들 마무리

--- common/optimizer.py
+++ common/optimizer.py
@@ -1,49 +1,100 @@
+# coding: utf-8
 import numpy as np
 
 class SGD:
-  
-  """확률적 경사 하강법 (Stochasic Gradient Descent)"""
-  
-  def __init__(self, lr=0.01):
-    self.lr = lr
-    
-  def update(self, params, grads):
-    for key in params.keys():
-      params[key] -= self.lr * grads[key]
-      
+
+    """확률적 경사 하강법（Stochastic Gradient Descent）"""
+
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.lr * grads[key] 
+
+
 class Momentum:
-  
-  """모멘텀 SGD"""
-  
-  def __init__(self, lr=0.01, momentum=0.9):
-    self.lr = lr
-    self.momentum = momentum
-    self.v = None
+
+    """모멘텀 SGD"""
+
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():                                
+                self.v[key] = np.zeros_like(val)
+                
+        for key in params.keys():
+            self.v[key] = self.momentum*self.v[key] - self.lr*grads[key] 
+            params[key] += self.v[key]
+
+
+class Nesterov:
+
+    """Nesterov's Accelerated Gradient (http://arxiv.org/abs/1212.0901)"""
+    # NAG는 모멘텀에서 한 단계 발전한 방법이다. (http://newsight.tistory.com/224)
     
-  def update(self, params, grads):
-    if self.v is None:
-      self.v = {}
-      for key, val in params.items():
-        self.v[key] = np.zeros_like(val)
-    
-    for key in params.keys():
-      self.v[key] = self.momentum*self.v[key] - self.lr*grads[key]
-      params[key] += self.v[key]
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+            
+        for key in params.keys():
+            self.v[key] *= self.momentum
+            self.v[key] -= self.lr * grads[key]
+            params[key] += self.momentum * self.momentum * self.v[key]
+            params[key] -= (1 + self.momentum) * self.lr * grads[key]
+
 
 class AdaGrad:
-  def __init__(self, lr=0.01):
-    self.lr = lr
-    self.h = None
-    
-  def update(self, params, grads):
-    if self.h is None:
-      self.h = {}
-      for key, val in params.items():
-        self.h[key] = np.zeros_like(val)
+
+    """AdaGrad"""
+
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        self.h = None
         
-    for key in params.keys():
-      self.h[key] += grads[key] * grads[key]
-      params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+            
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+
+
+class RMSprop:
+
+    """RMSprop"""
+
+    def __init__(self, lr=0.01, decay_rate = 0.99):
+        self.lr = lr
+        self.decay_rate = decay_rate
+        self.h = None
+        
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+            
+        for key in params.keys():
+            self.h[key] *= self.decay_rate
+            self.h[key] += (1 - self.decay_rate) * grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+
 
 class Adam:
 
@@ -68,13 +119,13 @@ class Adam:
         lr_t  = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)         
         
         for key in params.keys():
-            # self.m[key] = self.beta1*self.m[key] + (1-self.beta1)*grads[key]
-            # self.v[key] = self.beta2*self.v[key] + (1-self.beta2)*(grads[key]**2)
+            #self.m[key] = self.beta1*self.m[key] + (1-self.beta1)*grads[key]
+            #self.v[key] = self.beta2*self.v[key] + (1-self.beta2)*(grads[key]**2)
             self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
             self.v[key] += (1 - self.beta2) * (grads[key]**2 - self.v[key])
             
             params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
             
-            # unbias_m += (1 - self.beta1) * (grads[key] - self.m[key]) # correct bias
-            # unbisa_b += (1 - self.beta2) * (grads[key]*grads[key] - self.v[key]) # correct bias
-            # params[key] += self.lr * unbias_m / (np.sqrt(unbisa_b) + 1e-7)
+            #unbias_m += (1 - self.beta1) * (grads[key] - self.m[key]) # correct bias
+            #unbisa_b += (1 - self.beta2) * (grads[key]*grads[key] - self.v[key]) # correct bias
+            #params[key] += self.lr * unbias_m / (np.sqrt(unbisa_b) + 1e-7)
